a0a504700b7be17967141085d6cb96ed61763899
peteshko <peteshkus@gmail.com>
2024-10-20 16:29:55 +0300

    Count soldiers position and make them move

--- src/move_logic.rs
+++ src/move_logic.rs
@@ -1,7 +1,10 @@
-use crate::components::Condition;
-use crate::resources::WinSize;
+use crate::components::{Condition, Soldier};
+use crate::resources::{SpatialHash, WinSize};
 use bevy::prelude::*;
+use cgmath::Vector3;
 
+// TODO Use spatial hash to understand relative position of the soldier.
+//TODO
 pub fn movable_system(
     mut commands: Commands,
     win_size: Res<WinSize>,
