d1723806f7bcd05c7f932aa6556505ff73e22bd8
Igor Bologov <peteshkus@gmail.com>
2024-10-30 12:25:15 +0200

    center mass calc updated

--- src/moving_system_ai.rs
+++ src/moving_system_ai.rs
@@ -7,48 +7,53 @@ use std::io::{Write, BufReader};
 use crate::components::{Condition, Soldier, SpriteSize, Team, Squad};
 use crate::resources::{SpatialHash, WinSize, SquadVec};
 
-
-
-use std::rc::Rc;
-
-fn update_center_of_mass(
-    squads: &Vec<(Rc<Vec<Entity>>, u16)>, 
-    soldiers_query: Query<(&Soldier, &Team, &Squad)>
-) -> Vec<(Rc<Vec<Entity>>, (f32, f32), u16)> {
+fn update_center_of_mass<'a>(
+    squads: &'a Vec<(Vec<Entity>, u16)>, 
+    soldiers_query: &Query<(&Soldier, &Team, &Squad)>
+) -> Vec<(&'a Vec<Entity>, (f32, f32), u16)> {
     let mut centers_of_mass = Vec::new();
 
     for (squad, id) in squads {
         let mut sum_x = 0.0;
         let mut sum_y = 0.0;
 
-        for &entity in squad.iter() {
-            if let Ok((soldier, _team, _squad)) = soldiers_query.get(entity) {
-                sum_x += soldier.sh_coords.x as f32;
-                sum_y += soldier.sh_coords.y as f32;
+        if !squad.is_empty() {
+            for &entity in squad.iter() {
+                if let Ok((soldier, _team, _squad)) = soldiers_query.get(entity) {
+                    sum_x += soldier.sh_coords.x as f32;
+                    sum_y += soldier.sh_coords.y as f32;
+                }
             }
-        }
 
-        let count = squad.len() as f32;
-        if count > 0.0 {
-            let center_of_mass = (sum_x / count, sum_y / count);
-            centers_of_mass.push((Rc::clone(squad), center_of_mass, *id)); // Используем `Rc::clone`, что не копирует данные, а лишь увеличивает счетчик ссылок
+            let count = squad.len() as f32;
+            if count > 0.0 {
+                let center_of_mass = (sum_x / count, sum_y / count);
+                centers_of_mass.push((squad, center_of_mass, *id));
+            }
         }
     }
 
     centers_of_mass
 }
 
-pub fn analyze_circumstance(sh: Res<SpatialHash>, squads: Res<SquadVec>, soldiers_query: &Query<(&Soldier, &Team, &Squad)>){
+pub fn analyze_circumstance(
+    sh: Res<SpatialHash>, 
+    squads: Res<SquadVec>, 
+    soldiers_query: &Query<(&Soldier, &Team, &Squad)>
+) {
+    let centers_of_mass = update_center_of_mass(&squads.get_squads(), soldiers_query);
 
+    for (squad, (center_x, center_y), squad_id) in centers_of_mass {
+        println!(
+            "Центр масс отряда с идентификатором {} находится в координатах ({}, {})",
+            squad_id, center_x, center_y
+        );
 
-    for squad in squads{
-        let center_of_mass = update_center_of_mass(squad, soldiers_query);
-
-        
-
+        // Дополнительная логика для анализа и взаимодействия с отрядом
     }
-
 }
+
+
 
 pub fn modify_soldier_properties(){
 
